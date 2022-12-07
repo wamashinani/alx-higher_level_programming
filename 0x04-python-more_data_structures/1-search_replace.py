6950cc7cda4aa8e6fbc5dd1d7ea0211a53c62054
root <siele137@gmail.com>
2022-12-07 10:05:51 -0800

    check

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [[y ** 2 for y in x] for x in matrix]
+def search_replace(my_list, search, replace):
+    return [replace if num == search else num for num in my_list]
