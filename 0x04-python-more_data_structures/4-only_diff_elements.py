e601f7ed2ddac592b11665807b19432d0efc32e5
root <siele137@gmail.com>
2022-12-07 23:33:56 -0800

    check

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
-# The intersection set of A and B, denoted “A ∩ B” (read “A inter B”)
-# is the set of elements of A which are also elements of B.
-# We can use set_1.intersection(set_2) too.
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
+# The symmetrical difference of A and B, denoted “A Δ B”
+# (read “A delta B”) is the set of elements which belong
+# either to A or to B, but not to both at the same time.
+# We can use set_1.symetric_difference(set_2) too.
