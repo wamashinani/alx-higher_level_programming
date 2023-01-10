59512c3ba3e279efdc9c8ec0f3346bd0ed51e36a
root <siele137@gmail.com>
2023-01-10 05:50:54 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+number_of_lines = __import__('1-number_of_lines').number_of_lines
 
-
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+filename = "my_file_0.txt"
+nb_lines = number_of_lines(filename)
+print("{} has {:d} lines".format(filename, nb_lines))
