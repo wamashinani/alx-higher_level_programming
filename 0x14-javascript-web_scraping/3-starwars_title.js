fd4258efd27f41eaa466f55ebc5a4e4f424d3adb
wamashinani <siele137@gmail.com>
2023-07-25 10:41:20 -0700

    done for the second time

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
-
-request(starWarsUri, function (_err, _res, body) {
-  body = JSON.parse(body);
-  console.log(body.title);
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
